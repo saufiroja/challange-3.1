eb02c57a3f30566ca843b877dc7c6e635e732ed6
saufiroja <saufiroja08@gmail.com>
2021-10-09 20:31:10 +0800

    membuat fitur requireauth dan current user

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,11 +1,45 @@
-// const data = require("../data/db.json");
+const data = require("../data/db.json");
+const jwt = require("jsonwebtoken");
 
-// const requireAuth = (req, res) => {
-//   const id = req.body.id;
+const requireAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
 
-//   if(id) {
+  if (token) {
+    jwt.verify(token, "secret", (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.redirect("/login");
+      } else {
+        console.log(decodedToken);
+        next();
+      }
+    });
+  } else {
+    res.redirect("/login");
+  }
+};
 
-//   }
-// };
+// current user
+const currentUser = (req, res, next) => {
+  const token = req.cookies.jwt;
 
-// module.exports = { requireAuth };
+  if (token) {
+    jwt.verify(token, "secret", (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.locals.user = null;
+        next();
+      } else {
+        console.log(decodedToken);
+        let user = Object.values(decodedToken);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+module.exports = { requireAuth, currentUser };
