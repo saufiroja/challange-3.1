012715eacfed57a7b4a72672d02c4b1512f976e3
saufiroja <saufiroja08@gmail.com>
2021-10-09 14:16:13 +0800

    mengubah folder controllers

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const controller = require("../controller/authController");
+const controller = require("../controllers/authController");
 
 const router = Router();
 
